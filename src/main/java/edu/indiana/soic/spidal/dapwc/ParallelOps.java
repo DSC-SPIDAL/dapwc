28c126f1e7340e9093e936552fbab37cbec70d58
Saliya Ekanayake <esaliya@gmail.com>
2016-05-25 22:56:14 -0400

    NewCommit 45 (things are good) removed mapping and iterator style

--- src/main/java/edu/indiana/soic/spidal/dapwc/ParallelOps.java
+++ src/main/java/edu/indiana/soic/spidal/dapwc/ParallelOps.java
@@ -396,7 +396,7 @@ public class ParallelOps {
         worldProcsComm.barrier();
 
         for (int i = 0; i < worldProcsCount; ++i){
-            packets[i].copyFrom(i*packet.getExtent(), packet.getExtent(), mmapCollectiveXReadBytes);
+            packets[i].copyFrom(i*packet.getExtent(), packet.getArrayLength(), mmapCollectiveXReadBytes);
         }
     }
 
